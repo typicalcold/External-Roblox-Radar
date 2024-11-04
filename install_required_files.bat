a2781ee2febb2a411a2bd6ddbf24834dde06fe28
typicalcold <83438890+typicalcold@users.noreply.github.com>
2024-11-03 23:52:40 -0800

    Create install_required_files.bat

---
+++ install_required_files.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Installing tk...
+pip install tk
+
+echo Installing websockets...
+pip install websockets
+
+
+echo.
+echo For more information about tkinter, visit:
+echo https://docs.python.org/id/3.12/library/tkinter.html
+
+echo For more information about websockets, visit:
+echo https://pypi.org/project/websockets/
+pause
